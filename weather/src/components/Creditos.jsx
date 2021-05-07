dd02eedf01769680c49562c65597b007bc2c0c99
Daniel-Ignacio-Nieto <ignacionieto65@hotmail.com>
2021-05-07 19:54:25 -0300

    correcciones ortograficas

--- weather/src/components/Creditos.jsx
+++ weather/src/components/Creditos.jsx
@@ -8,7 +8,7 @@ const Credits = () => {
         <div className="Credits-Container-M">
             <div className="Credits-Container">
                 <h2>Créditos:</h2>
-                <p className="Credits-P">La pagina esta hecha gracias a la API del clima perteneciente a <a href="https://openweathermap.org/" target="_blank" rel="noreferrer" className="Credits-P-a">
+                <p className="Credits-P">La página esta hecha gracias a la API del clima perteneciente a <a href="https://openweathermap.org/" target="_blank" rel="noreferrer" className="Credits-P-a">
                     Open Weather Map</a></p>
                 <p className="Credits-P">La imagen de fondo la obtuve de <a href="https://fondosmil.com/" target="_blank" rel="noreferrer" className="Credits-P-a">Fondos Mil</a></p>
             </div>
--- weather/src/components/about.jsx
+++ weather/src/components/about.jsx
@@ -8,9 +8,9 @@ const About = () => {
         <div className="About-Container-M">
             <div className="About-Container">
                 <h2 >Sobre <span>Weather App</span>:</h2>
-                <p className="About-P">Esta Plataforma Web fue creada por <a href="https://github.com/Daniel-Ignacio-Nieto" target="_blank" rel="noreferrer" className="About-P-a">Ignacio Nieto</a> utilizando <a href="https://reactjs.org/" target="_blank" rel="noreferrer" className="About-P-a">React JS</a>.</p>
-                <p className="About-P">El objetivo sería obtener datos Meteorológicos de las ciudades que desees, estos datos son obtenidos gracias al uso de una API externa mediante el buscador de arriba. Una vez que ya tenemos esos datos los renderizamos en forma de tarjetas dinamicamente sobre la pantalla principal. Para lograr las distintas vistas de la pagina usamos el ruteo provisto por <a href="https://reactrouter.com/" target="_blank" rel="noreferrer" className="About-P-a">React Router</a>.</p>
-                <p className="About-P">Para obtner mas informacion sobre alguna ciudad, puedes pulsar el boton "Ver Detalles" que se encuentra en cada tarjeta.</p>
+                <p className="About-P">Esta Plataforma Web fué creada por <a href="https://github.com/Daniel-Ignacio-Nieto" target="_blank" rel="noreferrer" className="About-P-a">Ignacio Nieto</a> utilizando <a href="https://reactjs.org/" target="_blank" rel="noreferrer" className="About-P-a">React JS</a>.</p>
+                <p className="About-P">El objetivo sería obtener datos Meteorológicos de las ciudades que desees, éstos datos son obtenidos gracias al uso de una API externa mediante el buscador de arriba. Una vez que ya tenemos esos datos los renderizamos en forma de tarjetas dinámicamente sobre la pantalla principal. Para lograr las distintas vistas de la página usamos el ruteo provisto por <a href="https://reactrouter.com/" target="_blank" rel="noreferrer" className="About-P-a">React Router</a>.</p>
+                <p className="About-P">Para obtner más información sobre alguna ciudad, puedes pulsar el boton "Ver Detalles" que se encuentra en cada tarjeta.</p>
             </div>
         </div>
     );
